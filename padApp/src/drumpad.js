8639d89ae57b0a4f2caeb589abf7f698e23ab01c
otherotherjosh <itsotherjosh@gmail.com>
2026-06-20 10:34:10 +1200

    create mpc grid

--- padApp/src/drumpad.js
+++ padApp/src/drumpad.js
@@ -3,7 +3,7 @@ const MPC_PADS = [document.getElementById('mpc-pad-0')];
 const MPC_CONTAINER = document.getElementById('mpc-container');
 
 // create duplicates 
-for (let i = 1; i <= MPC_PAD_COUNT; i++) {
+for (let i = 1; i < MPC_PAD_COUNT; i++) {
   const pad = MPC_PADS[0].cloneNode(true);
   pad.id = `mpd-pad-${i}`;
   pad.childNodes.forEach(n => {
