37cfddac354c0000eb0bf61d3f5b8670bc0320f1
otherotherjosh <itsotherjosh@gmail.com>
2026-06-20 14:52:53 +1200

    listen to pad click events

--- padApp/src/drumpad.js
+++ padApp/src/drumpad.js
@@ -2,15 +2,34 @@ const MPC_PAD_COUNT = 12;
 const MPC_PADS = [document.getElementById('mpc-pad-0')];
 const MPC_CONTAINER = document.getElementById('mpc-container');
 
-// create duplicates 
-for (let i = 1; i < MPC_PAD_COUNT; i++) {
-  const pad = MPC_PADS[0].cloneNode(true);
-  pad.id = `mpd-pad-${i}`;
-  pad.childNodes.forEach(n => {
-    if (n.nodeType == Node.ELEMENT_NODE && n.tagName === 'SPAN') {
-      n.textContent = `PAD ${i}`;
-    }
+// TODO: run tone js noises
+
+const createPadPlayer = (index) => {
+  MPC_PADS[index].addEventListener("click", () => {
+    onPadPressed(index);
   });
-  MPC_CONTAINER.appendChild(pad);
-  MPC_PADS.push(pad);
+  // TODO: add sounds to pad
 }
+
+const onPadPressed = (index) => {
+  console.log(`PRESSED PAD ${index}`);
+}
+
+const init = () => {
+  createPadPlayer(0);
+  // create duplicates 
+  for (let i = 1; i < MPC_PAD_COUNT; i++) {
+    const pad = MPC_PADS[0].cloneNode(true);
+    pad.id = `mpd-pad-${i}`;
+    pad.childNodes.forEach(n => {
+      if (n.textContent === 'PAD 0') {
+        n.textContent = `PAD ${i}`;
+      }
+    });
+    MPC_CONTAINER.appendChild(pad);
+    MPC_PADS.push(pad);
+    createPadPlayer(i);
+  }
+}
+
+init();
