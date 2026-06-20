c587588ae47de9162e1dd15d832d04713cc8d8d4
Aaahtemis <churaa1@student.op.ac.nz>
2026-06-20 21:50:53 +1200

    adjust code for multiple buttons

--- piezoTesting/piezoTesting.ino
+++ piezoTesting/piezoTesting.ino
@@ -8,15 +8,18 @@ bool emaSeeded = false;
 float emAverage = 0;
 int sAverage[100];
 int lowerDeadZone = 100;
-int lowerMargin = 30;
+int lowerMargin = 0;
 
 float averageSmoothing = 0;
 
 int tickCount = 0;
 int tickCarryMoving = 1000;
-int tickCarrySimpleMod = 4;
+int tickCarrySimpleMod = 1000;
+
+bool isActive = false;
 
 void AddToStartOfArray(int value, int array[], int length, bool doPrint = false);
+float FindExponentialAverage(int value);
 
 void setup() {
   Serial.begin(9600);
@@ -25,7 +28,7 @@ void setup() {
   for (int i = lightStart; i <= lightEnd; i++) {
     pinMode(i, OUTPUT);
   }
-  for (int i = 0; i < sizeof(sAverage) / sizeof(sAverage[0]); i++){
+  for (int i = 0; i < sizeof(sAverage) / sizeof(sAverage[0]); i++) {
     sAverage[i] = lowerMargin;
   }
 
@@ -36,30 +39,48 @@ void setup() {
 void loop() {
   tickCount++;
 
-  int rawData = analogRead(input);
-  //Serial.println("raw data: " + String(rawData));
-  int safeValue = constrain(rawData, 0, 1023);
-  //Serial.println("safe data: " + String(safeValue));
-  float normalizedValue = map(safeValue, 0, 1023, 0, 100);
-  //Serial.println("normalized data: " + String(normalizedValue));
+  // if (tickCount % tickCarryMoving / tickCarrySimpleMod) {
+    if(true){
+      uint8_t analogPin = A0;
+    for (int i = 0; i < 6; i++) {
+      switch (i) {
+        case 0:
+          analogPin = uint8_t(A0);
+          break;
+        case 1:
+          analogPin = uint8_t(A1);
+          break;
+        case 2:
+          analogPin = uint8_t(A2);
+          break;
+        case 3:
+          analogPin = uint8_t(A3);
+          break;
+        case 4:
+          analogPin = uint8_t(A4);
+          break;
+        case 5:
+          analogPin = uint8_t(A5);
+          break;
+      }
+      int rawData = analogRead(A0);
 
-  float currentLightStage = map(normalizedValue, 0, 100, lightStart, lightEnd);
-  for (int i = lightStart; i <= lightEnd; i++) {
-    if (i < currentLightStage) {
-      digitalWrite(i, HIGH);
-    } else {
-      digitalWrite(i, LOW);
+      //Serial.println("raw data: " + String(rawData));
+      int safeValue = constrain(rawData, 0, 1023);
+      //Serial.println("safe data: " + Striuint8_tng(safeValue));
+      float normalizedValue = map(safeValue, 0, 1023, 0, 127);
+      //Serial.println("normalized data: " + String(normalizedValue));
+
+      if (normalizedValue > lowerMargin) {
+        Serial.println(String(0) + " sensor value : " + String(normalizedValue));
+        AddToStartOfArray(normalizedValue, sAverage, sizeof(sAverage) / sizeof(sAverage[0]));
+        //FindExponentialAverage(sAverage[0]);
+        digitalWrite(i + 2, HIGH);
+      } else {
+        digitalWrite(i + 2, LOW);
+      }
     }
   }
-
-  if (tickCount % tickCarryMoving / tickCarrySimpleMod) {
-    if (normalizedValue > lowerMargin){
-      AddToStartOfArray(normalizedValue, sAverage, sizeof(sAverage) / sizeof(sAverage[0]));
-      Serial.println(sAverage[0]);
-    }
-      FindExponentialAverage(sAverage[0]);
-  }
-
   if (tickCount >= tickCarryMoving) {  // inner loop #1
     tickCount = 0;
   }
@@ -94,4 +115,9 @@ float FindExponentialAverage(int value) {
   }
   emAverage = (value * averageSmoothing) + (emAverage * (1.0 - averageSmoothing));
   return emAverage;
+}
+
+void doAction() {
+
+  isActive = !isActive;
 }
